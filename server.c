a931163b6ee13a122fcf1e5bdb05c7d31238dd7b
psettle <patricksettle@hotmail.com>
2016-11-30 20:03:26 -0700

    asfad

--- server.c
+++ server.c
@@ -8,8 +8,15 @@ void runTest1() {
 	 pthread_t readers[500];
 	 pthread_t writers[100];
 	 
+	 //do all the reader tests
 	 for(u32 readerCount = 5; readerCount <= 500; readerCount += 5) {
-		 
+		 readerTestCase(reader_V1, writer_V2, &db1, readers, readerCount, writers, writerCount);
+
+	}
+
+}
+
+double readerTestCase(void *(*readerTask) (void *), void *(*writerTask) (void *), database_v1* db1, pthread_t* readers, u32 readerCount, pthread_t* writers, u32 writerCount) {
 #if VERBOSE
 		printf("Starting readerCount == %d\n", readerCount);
 #endif
@@ -17,13 +24,13 @@ void runTest1() {
 		 
 		 u32 index = 0;
 		 
-		 while(index < readerCount || index < 100) {
+		 while(index < readerCount || index < writerCount) {
 			if(index < readerCount) {
-				 pthread_create(&(readers[index]), NULL, reader_V1, (void*)&db1);
+				 pthread_create(&(readers[index]), NULL, readerTask, db1);
 			}
 			 
-			if(index < 100) {
-				`pthread_create(&(writers[index]), NULL, writer_V1, (void*)&db1);
+			if(index < writerCount) {
+				`pthread_create(&(writers[index]), NULL, writerTask, db1);
 			}
 			
 			index++;
@@ -41,13 +48,11 @@ void runTest1() {
 			 pthread_join(readers[i], &status);
 			
 			u32 nanoSeconds = *(u32*)status;
-			//printf("%u\n", nanoSeconds);
 			sumTurnaroundReader += nanoSeconds;
-
 			free(status);
 		 }
 		 
-		 for(u32 i = 0; i < 100; i++) {
+		 for(u32 i = 0; i < writerCount; i++) {
 			 pthread_join(writers[i], NULL);
 		 }
 		 
@@ -56,8 +61,4 @@ void runTest1() {
 #else
 		printf("%lf\n", sumTurnaroundReader / readerCount);
 #endif	
-
-
-	}
-
 }
